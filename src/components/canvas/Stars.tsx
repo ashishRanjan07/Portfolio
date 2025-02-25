af0550511b4439278387e190e9ec92e651194550
ashishRanjan07 <aviashishranjan@gmail.com>
2025-02-25 12:32:29 +0530

    fixed deployement Issue

--- src/components/canvas/Stars.tsx
+++ src/components/canvas/Stars.tsx
@@ -2,11 +2,11 @@ import { useState, useRef, Suspense } from "react";
 import { Canvas, useFrame } from "@react-three/fiber";
 import { Points, PointMaterial, Preload } from "@react-three/drei";
 import { random } from "maath";
-import { TypedArray } from "three";
+import * as THREE from "three"; // Add this import for THREE
 
 const Stars = (props: any) => {
   const ref = useRef<THREE.Points>();
-  const [sphere] = useState<TypedArray>(() =>
+  const [sphere] = useState<THREE.TypedArray>(() =>
     random.inSphere(new Float32Array(5001), { radius: 1.2 })
   );
 
